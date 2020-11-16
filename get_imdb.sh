060af5da04f4f951e1b70a55e7861f149553cfd8
Jonathan Rockower <jrockower@uchicago.edu>
2020-11-15 22:27:50 -0600

    Rename file to get imdb only

--- get_imdb.sh
+++ get_imdb.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-python3 BoxOfficeMojo.py
-
-ssh -i ~/.ssh/jrockower.pem hadoop@ec2-52-15-169-10.us-east-2.compute.amazonaws.com
-
 curl https://datasets.imdbws.com/name.basics.tsv.gz | gunzip | hdfs dfs -put - /tmp/jrockower/project/name_basics.tsv
 curl https://datasets.imdbws.com/title.akas.tsv.gz | gunzip | hdfs dfs -put - /tmp/jrockower/project/title_akas.tsv
 curl https://datasets.imdbws.com/title.basics.tsv.gz | gunzip | hdfs dfs -put - /tmp/jrockower/project/title_basics.tsv
