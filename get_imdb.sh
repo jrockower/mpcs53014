a33b5942720c9f3a79d03a24532366c3add54c0f
Jonathan Rockower <jrockower@uchicago.edu>
2020-11-22 22:33:49 -0600

    Update shell script to curl data directly to S3 rather than HDFS

--- get_imdb.sh
+++ get_imdb.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-curl https://datasets.imdbws.com/name.basics.tsv.gz | gunzip | hdfs dfs -put - /tmp/jrockower/project/name_basics.tsv
-curl https://datasets.imdbws.com/title.akas.tsv.gz | gunzip | hdfs dfs -put - /tmp/jrockower/project/title_akas.tsv
-curl https://datasets.imdbws.com/title.basics.tsv.gz | gunzip | hdfs dfs -put - /tmp/jrockower/project/title_basics.tsv
-curl https://datasets.imdbws.com/title.crew.tsv.gz | gunzip | hdfs dfs -put - /tmp/jrockower/project/title_crew.tsv
-curl https://datasets.imdbws.com/title.ratings.tsv.gz | gunzip | hdfs dfs -put - /tmp/jrockower/project/title_ratings.tsv
+curl https://datasets.imdbws.com/name.basics.tsv.gz | gunzip | aws s3 cp - s3://jrockower-mpcs53014/name_basics/name_basics.tsv
+curl https://datasets.imdbws.com/title.basics.tsv.gz | gunzip | aws s3 cp - s3://jrockower-mpcs53014/title_basics/title_basics.tsv
+curl https://datasets.imdbws.com/title.crew.tsv.gz | gunzip | aws s3 cp - s3://jrockower-mpcs53014/title_crew/title_crew.tsv
+curl https://datasets.imdbws.com/title.ratings.tsv.gz | gunzip | aws s3 cp - s3://jrockower-mpcs53014/title_ratings/title_ratings.tsv
