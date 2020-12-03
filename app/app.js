5b90d7ccb56e71d63ec5c0992ed7acbe16628040
Jonathan Rockower <jrockower@uchicago.edu>
2020-12-02 22:36:29 -0600

    Start to write review form

--- app/app.js
+++ app/app.js
@@ -47,7 +47,7 @@ app.get('/films-request-output.html', function (req, res) {
 		//Each film may have multiple writers/directors. This code creates a concatenated version to display in table.
 		var result = filmRecord;
 		var writer1 = result["writer1"];
-		var director1 = result["director1"]
+		const director1 = result["director1"];
 
 		if ("writer2" in filmRecord) {
 			if ("writer3" in filmRecord) {
@@ -73,7 +73,6 @@ app.get('/films-request-output.html', function (req, res) {
 		result['directors'] = all_directors
 
 		// Change format of number of votes to use comma format
-		// result['num_votes'] = parseFloat(filmRecord['num_votes']).toLocaleString('en')
 		result['num_votes'] = parseFloat(counterToNumber(filmRecord['num_votes']).toString()).toLocaleString('en')
 		console.log(result)
 		return result
@@ -102,18 +101,47 @@ app.get('/films-request-output.html', function (req, res) {
 		const week_formatted = week.substr(0, 4) + ' Week ' + week.substr(week.length - 2, 2)
 
 		hclient.table('jrockower_box_office_hbase').scan(
-			{filter: {type : "PrefixFilter", value: week}, maxVersions: 1}, (err, cells) => {
+			{filter: {type: "PrefixFilter", value: week}, maxVersions: 1}, (err, cells) => {
 				var fi = filminfo(cells, week);
+				// hclient.table('jrockower_ratings_hbase').row(filmRecord['title'] + ' (' + filmRecord['startyear'] + ')').get((error, value) => {
+				// 	console.log(parseFloat(counterToNumber(value[0]['$']).toString()).toLocaleString('en'))
+				// 	var votes = parseFloat(counterToNumber(value[0]['$']).toString()).toLocaleString('en')
+				// })
+				console.log(fi)
 				var template = filesystem.readFileSync("films-output.mustache").toString();
 				var html = mustache.render(template, {
-					filmInfo : fi,
-					filmname : film,
-					week : week_formatted
+					filmInfo: fi,
+					filmname: film,
+					week: week_formatted
 				});
 				res.send(html)
 			});
+	});
+});
+
+/* Send review to kafka */
+var kafka = require('kafka-node');
+var Producer = kafka.Producer;
+var KeyedMessage = kafka.KeyedMessage;
+var kafkaClient = new kafka.KafkaClient({kafkaHost: process.argv[5]});
+var kafkaProducer = new Producer(kafkaClient);
+
+app.get('/films-review.html', function (req, res) {
+	hclient.table('jrockower_film_keys_hbase').scan({ maxVersions: 1}, (err,rows) => {
+		var template = filesystem.readFileSync("review.mustache").toString();
+		var html = mustache.render(template, {
+			films : rows
+		});
+		res.send(html)
 	})
+});
+
+app.get('/review.html', function (req, res) {
+	const film = req.query['film'];
+	const review = req.query['review'];
+	console.log(req.query)
+	console.log(film);
+	console.log(review);
 
 })
-
 app.listen(port);
